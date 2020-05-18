7f68aba362fceb11a130e404da255fb0c3e98e33
nikabaikulovi <64590964+nikabaikulovi@users.noreply.github.com>
2020-05-18 09:53:28 +0400

    Add files via upload

---
+++ authorisation1.php
@@ -0,0 +1,71 @@
+<style>
+    .error {color: #ffffff;}
+    h2 {
+        background-color:#000000 ;
+        border: 1px solid red;
+        border-radius: 10px;
+        margin: 2px;
+        height: 100px;
+        color: white;
+        text-align: center;
+        font-size:40px;
+        font-family: "Times New Roman", Times, serif;
+        font-style: italic;
+    }
+    body {
+        background-image: linear-gradient(to right, #000000 , #600300);
+        margin: 0px;
+    }
+    span {
+        display: inline-block;
+        padding: 10px;
+
+
+    }
+    #form {
+        text-align: center;
+        display: block;
+        margin: 0px;
+    }
+    .button {
+        height: 30px;
+        width: 80px;
+    }
+    .input {
+        width:250px;
+        height:20px;
+    }
+</style>
+</head>
+<body>
+<?php
+$signinError="";
+if (isset($_POST["login"])) {
+    if($_POST["signinusername"]=="user" && $_POST["signinpassword"]=="user"){
+        session_start();
+        $_SESSION["Username"]=$_POST["signinusername"];
+        $_SESSION["Password"]=$_POST["signinpassword"];
+        header("Location:welcometosession.php ");
+    }
+    else {
+        $signinError="Make sure all fields are filled in correctly";
+    }
+}
+
+
+?>
+
+
+<h2><br>SIGN IN</h2>
+<div id="form">
+    <form method="post" action="Lauthorisation.php">
+        <span><b>Username:</b></span> <input class="input" type="text" name="signinusername">
+        <span><b>Password:</b></span> <input class="input" type="password" name="signinpassword">
+        <span class="error"><b><?php echo $signinError; ?></b> </span>
+        <input type="submit" name="login" value="Login" class="button">
+        <br>
+    </form>
+</div>
+
+</body>
+</html>
---
+++ singin.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <style>
+        .error {color: #ffffff;}
+        h2 {
+            background-color:#000000 ;
+            border: 1px solid red;
+            border-radius: 10px;
+            margin: 2px;
+            height: 100px;
+            color: white;
+            text-align: center;
+            font-size:40px;
+            font-family: "Times New Roman", Times, serif;
+            font-style: italic;
+        }
+        body {
+            background-image: linear-gradient(to right, #000000 , #600300);
+            margin: 0px;
+        }
+        span {
+            display: inline-block;
+            padding: 10px;}
+        #form {
+            text-align: center;
+            display: block;
+            margin: 0px;
+        }
+        .button {
+            width: 80px;
+            height: 30px;
+
+        }
+        .input {
+            width:250px;
+            height:20px;
+        }
+    </style>
+</head>
+<body>
+<?php
+session_start();
+if (isset($_post["logout"])){
+    session_unset();
+    session_destroy();
+}
+?>
+
+
+<?php
+if(isset($_FILES['image'])){
+    $errors= array();
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+    $file_type = $_FILES['image']['type'];
+    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+    $extensions= array("jpg","jpeg","png");
+
+    if(in_array($file_ext,$extensions)=== false){
+        $errors[]="only jpg,jpeg,png acctenshion alowed.";
+    }
+
+    if($file_size > 2100000) {
+        $errors[]='max size 2Mb';
+    }
+
+    if(empty($errors)==true) {
+        move_uploaded_file($file_tmp,"images/".$file_name);
+        echo "Success";
+    }else{
+        print_r($errors);
+    }
+}
+?>
+
+<h2><br>Welcome</h2>
+<div class="form">
+    <span>Your Username is:<?php echo $_SESSION["Username"] ?></span>
+    <spna>Your Password is:<?php echo $_SESSION["Password"] ?></span>
+</div>
+<div class="form">
+    <form action="IMAGE.php" method="post" enctype="multipart/form-data">
+        <span>You can upload Your image<br></span>
+        <input type = "file" name = "image" >
+        <input type = "submit" value = "Upload" name="submit">
+    </form>
+
+    <img src = "<?php print $_SESSION['img']; ?>" alt = "IMG">
+
+</div>
+<form method="POST" action="Lauthorisation.php">
+    <input type="submit" value="LOGOUT" name="logout">
+</form>
+</body>
+</html>
